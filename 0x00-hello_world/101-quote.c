b1368aef07b58a7e8ccf0614ffcfec79fd59f0f3
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2022-09-08 21:54:38 +0100

    tryna obey betty-style for the 3rd 😮‍💨😮‍💨

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <unistd.h>
-/**
+/*
 main - Entry point
 Return:1
- */
+*/
 int main(void)
 {
 char ch[] = "and that piece of art is
--- 0x00-hello_world/testBetty.c
+++ 0x00-hello_world/testBetty.c
@@ -5,8 +5,7 @@ int main(void)
 int a = 5;
 int b = 4;
 
-for (a < b;;)
-{
+for (a < b;;){
     printf("%d\n", a++);
 }
 }
