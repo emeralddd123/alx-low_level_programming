32d75491aee813fbb705912f85a8d78b838ba7b4
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2022-09-13 14:38:41 +0100

    advanced task solutions

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
-
 /**
-* main - print the first 50 fibonacci numbers, starting with 1 and 2
-* followed by a new line
-* Return: 0 if successful
-*/
+ * main - main block
+ * Description: computes and prints even  number < 4,000,000
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
 int main(void)
 {
-long int i, j, k, next;
+    int a = 0, b = 1, next = 0;
+    int sum = 0;
 
-j = 1;
-
-k = 2;
-
-for (i = 1; i <= 50; ++i)
-{
-if (j != 20365011074)
-{
-printf("%ld, ", j);
-}
-else
-{
-printf("%ld\n", j);
-}
-next = j + k;
-j = k;
-k = next;
-}
-
-return (0);
+    while (next < 4000000)
+    {
+        next = a + b;
+        a = b;
+        b = next;
+        if (next % 2 == 0)
+            sum += next;
+    }
+    printf("%i\n", sum);
+    return (0);
 }
