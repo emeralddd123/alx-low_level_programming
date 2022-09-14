07af6bcffbc24c35bb34a0ada7f8a1f9c64a4626
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2022-09-14 03:10:10 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,9 @@
 #include"main.h"
 
 /**
- * print_times_table - prints time table of n
- *
- * @n: takes number input
-*/
+ *print_times_table - prints time table of n
+ *@n: takes number input
+ */
 
 void print_times_table(int n)
 {
