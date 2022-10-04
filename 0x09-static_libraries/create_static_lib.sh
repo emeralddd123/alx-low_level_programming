b549eebc929a7ec06d1b5ba00d261b52962e1c5f
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2022-10-04 12:52:38 +0100

    kj

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -3,10 +3,3 @@ gcc -c *.c
 <<<<<<< HEAD
 <<<<<<< HEAD
 ar -rcs libmy.a *.o
-=======
-ar -rcs liball.a *.o
->>>>>>> parent of 6c7458d (remove mistake files)
-
-=======
-ar -rcs libmain.a *.o
->>>>>>> 4f29fb219da0dce8e3c0f3dd4c923a41a3b0016c
