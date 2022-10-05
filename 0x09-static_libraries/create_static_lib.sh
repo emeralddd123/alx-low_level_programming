17b55db9fe6ab68ca1eb1757cada527d44643e53
emeralddd123 <93847541+emeralddd123@users.noreply.github.com>
2022-10-05 16:58:50 +0100

    fix

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-gcc -c *.c
-ar -rcs liball.a *.o
+#this script creates a static library called "liball.a"
+gcc -c -Wall -Werror -Wextra -pedantic -std=gnu89 *.c
+ar -rc liball.a *.o
+ranlib liball.a
